184e19994a88b501e9c787ed7ffcd156a0dd3bfa
Francois Ducatillon <francois.ducatillon@decathlon.com>
2017-11-23 14:03:48 +0100

    do exercice version 1

--- 02_programming_fundamentals/03_algorithms_day_1/03_play_with_conditional_statement/01_can_i_drink_alcohol.js
+++ 02_programming_fundamentals/03_algorithms_day_1/03_play_with_conditional_statement/01_can_i_drink_alcohol.js
@@ -14,3 +14,27 @@
 // it is expected to see failures for these tests:
 // "Frieda|Francis should be inside an object frieda|francis".
 // All the other tests should work.
+// Create two objects, `francis`, `frieda` with the fields : `age` (an integer) and `name`, (obviously a string)
+const frieda = {age:22,name:"Frieda"};
+const francis = {age:17,name:"Francis"};
+
+// Create two variables `canFriedaDrinkAlcohol` and `canFrancisDrinkAlcohol` to answer, respectively, to the question "Can he / she drink
+// alcohol.".
+// These variables should stay correct if we modify the ages of the objects `francis` and `frieda`.
+
+let canFriedaDrinkAlcohol = false;
+let canFrancisDrinkAlcohol = false;
+
+if(frieda.age >= 18) {
+  canFriedaDrinkAlcohol = true;
+  console.log("Frieda can drink alcohol");
+} else {
+  console.log("Frieda cannot drink alcohol");
+}
+
+if(francis.age >= 18) {
+  canFrancisDrinkAlcohol = true;
+  console.log("Francis can drink alcohol");
+} else {
+  console.log("Francis cannot drink alcohol");
+}
