02a1175d137ad39d5410b030e489978076eb9cf1
Francois Ducatillon <francois.ducatillon@decathlon.com>
2017-11-24 18:58:21 +0100

    presque fini

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
@@ -1,12 +1,17 @@
 // filter takes an array of integer and a string with either odd or even
 // such as filter([1, 2, 3, 4, 5], 'even') returns [2, 4]
 
+
+
+const myArray = [1, 2, 3, 4, 5];
+const str = "";
+
 function filter(myArray, str) {
   // Your code here
+  const newArray = [];
+
   for(let i=0;i<myArray.length;i++){
     //
-    const newArray = [];
-
     if(str === "even"){
       if(myArray[i]%2===0){
         //console.log("even");
@@ -15,8 +20,8 @@ function filter(myArray, str) {
       else {
       //  console.log("odd");
       }
-      return newArray;
     }
+
     else if(str === "odd"){
       //
       if(myArray[i]%2===0){
@@ -26,13 +31,16 @@ function filter(myArray, str) {
       //  console.log("odd");
         newArray.push(myArray[i]);
       }
-      return newArray;
     }
   }
+
+  return newArray;
 }
 
-const myArray = [1, 2, 3, 4, 5];
-const str = "";
+const output = filter(myArray, "even");
+console.log(output);
+const output2 = filter(myArray, "odd");
+console.log(output2);
 
 // do not remove this line, it is for tests
 module.exports = filter;
