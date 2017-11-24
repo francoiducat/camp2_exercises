6fcdfaeb378f61e7bb0dc8704437cb6b1353ed68
Francois Ducatillon <francois.ducatillon@decathlon.com>
2017-11-24 15:15:06 +0100

    function range SUCCEEDED

--- 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
+++ 02_programming_fundamentals/04_algorithms_day_2/01_range_function/01_range.js
@@ -4,9 +4,28 @@
 // This function should work in both ascending or descending order.
 
 // Complete this function.
-function range() {
 
+function range(number, endNumber) {
+
+  if (number < endNumber) {
+    const theArray = [];
+    for (let i = number; i < endNumber + 1; i++) {
+      theArray.push(i);
+    }
+    return theArray;
+  } else {
+    const theArray = [];
+    for (let i = number; i > endNumber - 1; i--) {
+      theArray.push(i);
+    }
+    return theArray;
+  }
 }
+
+
+
+
+
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
