2e85dbe3030c27fa3545c1f4c94cc0f145712598
Francois Ducatillon <francois.ducatillon@decathlon.com>
2017-11-27 14:29:51 +0100

    object and callback as parameter

--- 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
+++ 02_programming_fundamentals/05_callbacks/02_create_callbacks/create_callback.js
@@ -2,8 +2,11 @@ const log = (text) => console.log(text);
 
 // A function that takes two parameters, this first one being an object like below
 // and the second one being a callback function.
-const getInput = (____, ____) => {
-  // When finished, we want to log the key params...
+const getInput = (user1, callback) => {
+  // When finished, we want to call the callback function with the key param...
+  callback(user1.key);
 };
+
+
 
 getInput({user: "Lorem", key: "arrow_up"}, log);
