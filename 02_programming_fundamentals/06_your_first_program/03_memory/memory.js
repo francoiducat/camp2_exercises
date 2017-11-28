29026fa1ee9c6abccae87f46e15f13135a095b9d
Francois Ducatillon <francois.ducatillon@decathlon.com>
2017-11-28 17:00:19 +0100

    package-lock.json modified after npm install in 02_number_game_with_stats

--- 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
+++ 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
@@ -1,2 +1,18 @@
+//
+// The memory is a game where you have turned-over cards in front of you and you must find pairs.
+// You can return two cards at a time, if they are the same, they stay visible, if not, they go back to being flipped over.
+//
+// Instead of cards, we will be using emojis and you can start with these:
+//
+// const cards = ["🐰", "🐰", "🎃", "🎃", "🌲","🌲"];
+//
+// For this progran, you will need to install packages in the exercise directory. You do that with the command npm install.
+// It will allow you to use the clear() function that will clear your terminal (you need to show only the cards you found).
+//
+
+
 // This function will clear the terminal when called
 const clear = require("cli-clear");
+
+
+const cards = ["🐰", "🐰", "🎃", "🎃", "🌲","🌲"];
