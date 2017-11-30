8c648dc49bd3a6e7111d6522b34781a8b5c3e6a0
Francois Ducatillon <francois.ducatillon@decathlon.com>
2017-11-30 16:31:24 +0100

    Playing with a function with 2 parameters : an array and a function

--- 02_programming_fundamentals/04_algorithms_day_2/05_map_double/02_map_double.js
+++ 02_programming_fundamentals/04_algorithms_day_2/05_map_double/02_map_double.js
@@ -4,13 +4,30 @@
 // - an array
 // - a function
 //
-// this function apply the function passed as a parameter on each values of the array (also passed as a parameter).
+// this function apply the function passed as a parameter on each values of the array
+//(also passed as a parameter).
 //
 // Finally, test map with an array of your choice and the double function.
 //
 // WARNING: You're not allowed to use `Array.map`!
 
 // Your code here...
+function double(number){
+  return parseInt(number,10)*2;
+}
+
+function map(anArray,fn){
+  const newArray = [];
+  for(let i=0;i<anArray.length;i++){
+    newArray.push(fn(parseInt(anArray[i],10)));
+  }
+  return newArray;
+}
+
+const theArray = [ 1, 2, 3, 4, 5];
+
+const output = map(theArray, double);
+console.log(output);
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
