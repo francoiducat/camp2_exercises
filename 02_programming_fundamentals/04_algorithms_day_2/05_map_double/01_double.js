2c7a6a7dab9df3c11b0967215ca82915724549dd
Francois Ducatillon <francois.ducatillon@decathlon.com>
2017-11-30 16:22:25 +0100

    Playing with array push again

--- 02_programming_fundamentals/04_algorithms_day_2/05_map_double/01_double.js
+++ 02_programming_fundamentals/04_algorithms_day_2/05_map_double/01_double.js
@@ -1,7 +1,19 @@
-// Create a function `double` which take an array of integer as parameter and return a new array with all values doubled.
+// Create a function `double` which take an array of integer as parameter
+//and return a new array with all values doubled.
 // WARNING: You're not allowed to use `Array.map`!
 
 // Your code here...
+
+const myArray = [ 1, 2, 3, 4, 5];
+
+function double(anArray) {
+  const newArray = [];
+  for (let i=0;i<anArray.length;i++){
+    newArray.push(parseInt(anArray[i],10)*2);
+  }
+
+  return newArray;
+}
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
