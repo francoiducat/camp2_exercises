fd24920f92a54f89974bbbdad7c6eabd59b00b1c
Francois Ducatillon <francois.ducatillon@decathlon.com>
2017-11-30 14:27:12 +0100

    playing with this function with a coffee machine

--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -1,13 +1,41 @@
 // See Sparta courses for the exercise summary
 
+
+
+
+const machine = {
+  litersOfCoffee: 0,
+  fillWithLitersOfCoffee: function(param) {
+    return this.litersOfCoffee = this.litersOfCoffee + param;
+  },
+  expresso: function() {
+    if (this.litersOfCoffee >= 0.08) {
+      this.litersOfCoffee = this.litersOfCoffee - 0.08;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  longCoffee: function() {
+    if (this.litersOfCoffee >= 0.15) {
+      this.litersOfCoffee = this.litersOfCoffee - 0.15;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 // Coffee Machine usage. Insert your code above this comment
 
 machine.fillWithLitersOfCoffee(10);
+
 console.log(machine.expresso()) // => true
 console.log(machine.litersOfCoffee) // => 9.92
 console.log(machine.longCoffee()) // => true
 console.log(machine.litersOfCoffee) // => 9.77
-// some more people ordering coffee here
+console.log(machine.longCoffee()) // => true
+
+
 console.log(machine.litersOfCoffee) // => 0.02
 console.log(machine.expresso()) // => false
 
